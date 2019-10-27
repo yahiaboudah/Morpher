e6b3c48029e817916b471aadff8f6a6a317041af
Yahia Boudah <yahiabouda@hotmail.com>
2019-10-27 20:04:30 +0100

    fixed the getFiles function

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -1,67 +1,115 @@
+//@include "../../Logger/logger.jsx";
 
-JS_JSX_PATTERN = /\.(js|jsx)$/i;
+logger = new Logger({
+    loggerName: "newLogger",
+    enabled: false
+});
 
 function constructor(instance){
     parent = instance.constructor.toString().split(" ")[1].slice(0,-2);
     return parent;
 }
 
-function getFiles(folder,subFolders){
-    
-    fetchedFiles = [];
 
-    subFolders = subFolders || true;
+function getFiles(folderPath,mask){
+
+    O = {
+        fileQueue: [],
+        folderQueue: [],
+        fileType: "File",
+        folderType: "Folder",
+        getF: function(dirPath,type){
+                finalArray = [];
+                var folder = Folder(dirPath);
+                var files = folder.getFiles();
+                for(i=0;i<files.length;i++){
+                    switch (type) {
+                        case "Folder":
+                            if(files[i] instanceof Folder){
+                                finalArray.push(files[i]);
+                            }
+                            break;
+                        case "File":
+                                if(files[i] instanceof File){
+                                    if(mask == undefined) {
+                                        finalArray.push(files[i]);
+                                    }else{
+                                        if(files[i].fullName.search(mask) != -1){
+                                            finalArray.push(files[i]);
+                                        }
+                                    }
+                                }
+                            break;        
+                        default:
+                            break;
+                    }
+                }
+                return finalArray;
+        }
+    };
     
-    var files = folder.getFiles();
+    var files = O.getF(folderPath,O.fileType);
+    Array.prototype.push.apply(O.fileQueue,files);
+
+    O.folderQueue = O.getF(folderPath,O.folderType);
     
-    for(i=0;i<files.length;i++){
+    while(O.folderQueue.length > 0){
+        folderNow = O.folderQueue.pop();
         
-        if(files[i] instanceof File){
-                
-            if(files[i].fullName.search(JS_JSX_PATTERN) != -1){
-                fetchedFiles.push(files[i]);
-            } 
+        filesInFolder = O.getF(folderNow,O.fileType);
+        Array.prototype.push.apply(O.fileQueue,filesInFolder);
         
-        }else if(files[i] instanceof Folder){
-        
-            if(subFolders) getFiles(files[i],subFolders);
-        
-        }  
+        foldersInFolder = O.getF(folderNow,O.folderType);
+        Array.prototype.push.apply(O.folderQueue,foldersInFolder);
     }
-    return fetchedFiles;
+
+    return O.fileQueue;
+
 }
 
-function include(list,allSubFolders){
-    
-    incFiles = [];
-    
+
+function include(list){
+
+    O = {
+        JS_JSX_PATTERN : /\.(js|jsx)$/i,
+        incFiles :[]
+    }
+
     for(var i=0;i<list.length;i++){
         s = list[i];
         if(typeof s != "string"){
             throw new Error("File or directory name must be a string.");
         }
-        if(s.search(JS_JSX_PATTERN) != -1){
+        if(s.search(O.JS_JSX_PATTERN) != -1){
             
             file = File(s);
             if(!file.exists) throw Error("File: "+s+" does not exist.");
-            incFiles.push(file);
-        
+            O.incFiles.push(file);
+            
         }else if(s.search(/\/$/) != -1){
 
             folder = Folder(s);
             if(!folder.exists) throw Error("Folder: "+s+"does not exist.");
-            fetchedFiles = getFiles(folder,allSubFolders);
-            incFiles.push(fetchedFiles[0],fetchedFiles[1]);          
-
+            allFilesInFolder = getFiles(folder,O.JS_JSX_PATTERN);
+            Array.prototype.push.apply(O.incFiles,allFilesInFolder);
+            
         }else{
             throw new Error("File or folder name invalid");
         }
         
     }
-    alert(incFiles);
-    for(i=0;i<incFiles.length;i++) {
-        
-        //$.evalFile(incFiles[i]);
+    
+    alert(O.incFiles.length);
+    for(i=0;i<O.incFiles.length;i++) {
+        $.evalFile(incFiles[i]);
     }
 
 }
+
+include([
+    "../test5/"
+]);
+
+
+
+
